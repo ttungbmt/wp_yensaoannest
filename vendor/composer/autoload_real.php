9f79a212f58b2556e2d109e4c68ab19c6dab9c34
ttungbmt <ttungbmt@gmail.com>
2020-11-11 03:07:46 +0000

    Update composer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,9 +13,6 @@ class ComposerAutoloaderInit023f1af560a511a9d2d510affb2333af
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
